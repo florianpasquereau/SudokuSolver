61e304f57a0ac41158a26fa217bec5a01df42c0e
H295226 <FlorianPasquereau.Pasquereau@Honeywell.com>
2020-12-07 11:56:48 +0100

    managing the number of loop(s) through main's argument

--- src/main/java/SudokuGridScrapper/Main.java
+++ src/main/java/SudokuGridScrapper/Main.java
@@ -8,14 +8,20 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Main {
-    public static void main(String[] argv) throws Exception {
+
+    private static void helper() {
+        System.out.println("./SudokuSolver <number loop>");
+        System.out.println("<number loop> must be positive");
+        System.exit(-1);
+    }
+
+    private static void loop(int numberLoop) throws Exception {
         GridManager gridManager = new GridManager();
         Grid grid;
         GridSolver gridSolver = new GridSolver();
         LocalDateTime start;
         Duration period;
         Long average = 0L;
-        int numberLoop = 10;
 
         for (int i = 0; i < numberLoop; i++) {
             start = LocalDateTime.now();
@@ -27,5 +33,14 @@ public class Main {
         }
         gridManager.close();
         System.out.println("Average time : " + (average / numberLoop) + "ms");
+    }
+
+    public static void main(String[] argv) throws Exception {
+        int numberLoop = 0;
+
+        if (argv.length <= 0 || (numberLoop = Integer.parseInt(argv[0])) <= 0) {
+            Main.helper();
+        }
+        Main.loop(numberLoop);
     }
 }
